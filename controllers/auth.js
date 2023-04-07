c8e9141fed11c0cc8087aa9a893c54983f4a6b39
Mohammed Saeid <43301747+MoSaeid@users.noreply.github.com>
2023-04-07 09:43:34 +0200

    add basic auth

---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const User = require('../models/users');
+
+module.exports.renderRegister = (req, res) =>{
+    res.render('auth/register')
+}
+
+module.exports.register = async(req, res) =>{
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.flash('success', 'well done, you are member now')
+    res.redirect('/login')
+}
+
+module.exports.renderLogin = (req, res) =>{
+    res.render('auth/login')
+}
+
+module.exports.login = async(req, res) =>{
+    req.flash('success', 'welcome back')
+    res.redirect('/')
+}
+
+module.exports.logout = (req, res) => {
+    req.logout((err)=>{
+        if(err){ return next(err)}
+        req.flash('success', "Goodbye!");
+        res.redirect('/');
+    })
+}
